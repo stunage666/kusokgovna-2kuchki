fa59f718639b824ad5f3e5636046df219b09757d
Unknown <gic0@bk.ru>
2017-07-25 14:02:50 +0400

    еще херни

--- index.php
+++ index.php
@@ -60,7 +60,17 @@ elseif ($_SESSION['role'] === "Operator") {
 
 ?>
 
-
+фывфывфывфывфывфв
+фывфывфыв<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
 <!DOCTYPE html>
 <html lang="ru">
 <head>
